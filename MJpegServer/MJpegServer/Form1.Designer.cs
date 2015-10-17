a64faf796b3fd81c25ab64cda6888e7b4d98fbc0
KeyMove <lzcmail13@gmail.com>
2015-10-17 15:35:04 +0800

    1.0

---
+++ MJpegServer/MJpegServer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MJpegServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SendImg = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.ButtonMode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SendImg
+            // 
+            this.SendImg.Location = new System.Drawing.Point(174, 12);
+            this.SendImg.Name = "SendImg";
+            this.SendImg.Size = new System.Drawing.Size(75, 23);
+            this.SendImg.TabIndex = 0;
+            this.SendImg.Text = "刷新文件";
+            this.SendImg.UseVisualStyleBackColor = true;
+            this.SendImg.Click += new System.EventHandler(this.SendImg_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "开机自动运行";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // ButtonMode
+            // 
+            this.ButtonMode.AutoSize = true;
+            this.ButtonMode.Location = new System.Drawing.Point(172, 38);
+            this.ButtonMode.Name = "ButtonMode";
+            this.ButtonMode.Size = new System.Drawing.Size(53, 12);
+            this.ButtonMode.TabIndex = 2;
+            this.ButtonMode.Text = "普通模式";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 52);
+            this.Controls.Add(this.ButtonMode);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.SendImg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MJpegServer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SendImg;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label ButtonMode;
+    }
+}
+
---
+++ MJpegServer/MJpegServer/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KMButton;
+using Microsoft.Win32;
+
+namespace MJpegServer
+{
+    public partial class Form1 : Form
+    {
+
+        MJPEGStream MjpegServer=new MJPEGStream();
+        HTTP WebServer = new HTTP(80);
+        Bitmap map;
+        Graphics g;
+
+        Timer UpdateImage = new Timer();
+
+        int WindowW= System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+        int WindowH= System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+
+        int updatevalue=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            map = new Bitmap(WindowW, WindowH);
+            g = Graphics.FromImage(map);
+            g.Clear(Color.Black);
+            g.Dispose();
+            ScreenCap.Init(WindowW, WindowH, true);
+            if (Input.Init())
+                ButtonMode.Text = "DM模式";
+            WebServer.ServerStart();
+            WebServer.RegisterPost("cmd:", (string v,Socket s) => {
+                s.Disconnect(true);
+                s.Close();
+                s.Dispose();
+                string[] list=v.Split(',');
+                if (list[1][0] == 'Q')
+                {
+                    try
+                    {
+                        int value=int.Parse(list[1].Substring(2));
+                        MjpegServer.Quality = value;
+                    }
+                    catch { }
+                }
+                if (list[2][0] == 'S')
+                {
+                    try
+                    {
+                        int value = int.Parse(list[2].Substring(2));
+                        double sc = (double)value / 100;
+                        ScreenCap.Init((int)(WindowW * sc), (int)(WindowH * sc), true);
+                    }
+                    catch { }
+                }
+                if (list[3][0] == 'F')
+                {
+                    try
+                    {
+                        int value = int.Parse(list[3].Substring(2));
+                        if (value < 1) value = 1;
+                        if (value > 30) value = 30;
+                        updatevalue = 1000/value;
+                    }
+                    catch { }
+                }
+            });
+            WebServer.RegisterPost("mouse:", (string v, Socket s) => {
+                s.Disconnect(true);
+                s.Close();
+                s.Dispose();
+                int px, py;
+                try
+                {
+                    switch (v[6])
+                    {
+                        case 'd':
+                            switch (v[8] - '0')
+                            {
+                                case 0:
+                                    Input.MouseInput(0, 0, Input.MouseFlag.LeftDown);
+                                    break;
+                                case 1:
+                                    break;
+                                case 2:
+                                    Input.MouseInput(0, 0, Input.MouseFlag.RightDown);
+                                    break;
+                            }
+                            break;
+                        case 'u':
+                            switch (v[8] - '0')
+                            {
+                                case 0:
+                                    Input.MouseInput(0, 0, Input.MouseFlag.LeftUp);
+                                    break;
+                                case 1:
+                                    break;
+                                case 2:
+                                    Input.MouseInput(0, 0, Input.MouseFlag.RightUp);
+                                    break;
+                            }
+                            break;
+                        case 'm':
+                            string[] pv = v.Substring(8).Split('|');
+
+                            px = int.Parse(pv[0]);
+                            py = int.Parse(pv[1]);
+                            double wx = int.Parse(pv[2]);
+                            double wy = int.Parse(pv[3]);
+                            wx = WindowW / wx;
+                            wy = WindowH / wy;
+                            px = (int)(px * wx);
+                            py = (int)(py * wy);
+                            Input.MouseInput(px, py, Input.MouseFlag.MoveTo);
+                            break;
+                    }
+                }
+                catch { }
+            });
+            WebServer.RegisterPost("key:", (string v, Socket s) => {
+                s.Disconnect(true);
+                s.Close();
+                s.Dispose();
+                int vk = GetVkCode(v.Substring(6));
+                if (v[4] == '1')
+                {
+                    Input.KeyInput(vk, Input.KeyFlag.Down);
+                }
+                else
+                {
+                    Input.KeyInput(vk, Input.KeyFlag.Up);
+                }
+            });
+            WebServer.RegisterGet((string v, Socket s) => {
+                if(v.Contains("Stream"))
+                {
+                    MjpegServer.SendImage(s, map);
+                    MjpegServer.AddSocket(s);
+                }
+            });
+
+            UpdateImage.Tick += (object s, EventArgs ev) => {
+                //g.Clear(Color.White);
+                //g.DrawString(DateTime.Now.ToString().Replace(" ", "\r\n"), Font, Brushes.Black, 0, 0);
+                if (MjpegServer.Clients > 0)
+                {
+                    try { 
+                    MjpegServer.WriteImage(ScreenCap.GetImage());
+                    }
+                    catch { }
+                }
+                if (updatevalue != 0)
+                {
+                    ((Timer)s).Interval = updatevalue;
+                    updatevalue = 0;
+                }
+            };
+            UpdateImage.Interval = 1000/5;
+            UpdateImage.Start();
+
+            base.FormClosed +=(object sx,FormClosedEventArgs ec) =>
+            {
+                System.Environment.Exit(0);
+            };
+        }
+
+        byte GetVkCode(string s)
+        {
+            string str = s.ToUpper();
+            switch (str)
+            {
+                case "TAB": return 9;
+                case "BACK": return 8;
+                case "RETURN": return 0x0d;
+                case "SHIFT": return 0x10;
+                case "CTRL": return 0x11;
+                case "ALT": return 0x12;
+                case "END": return 0x23;
+                case "HOME": return 0x24;
+                case "ARROWLEFT": return 0x25;
+                case "ARROWUP": return 0x26;
+                case "ARROWRIGHT": return 0x27;
+                case "ARROWDOWN": return 0x28;
+                case "INSERT": return 0x2d;
+                case "DELETE": return 0x2E;
+                default: return (byte)str.ToCharArray()[0];
+            }
+        }
+
+        private void StartServer_Click(object sender, EventArgs e)
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+
+            int height = Screen.PrimaryScreen.Bounds.Height;
+
+            string path = System.IO.Directory.GetCurrentDirectory() + "\\test.jpg";
+
+            Bitmap a = new Bitmap(width, height);
+            Graphics gc = Graphics.FromImage(a);
+            gc.CopyFromScreen(0, 0, 0, 0, new Size(width, height));
+            EncoderParameters p = new EncoderParameters(1);
+            p.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 70L);
+            //p.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.ColorDepth, 18L);
+            //p.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (long)EncoderValue.CompressionCCITT4);
+            ImageCodecInfo info=null;
+            foreach (ImageCodecInfo v in ImageCodecInfo.GetImageEncoders())
+            {
+                if (v.MimeType == "image/jpeg")
+                {
+                    info = v;
+                    break;
+                }
+            }
+            try {
+                a.Save(path,info,p);
+            }
+            catch(Exception ex) { }
+            //a.UnlockBits(data);
+            a.Dispose();
+            //b.Dispose();
+            gc.Dispose();
+        }
+
+        private void SendImg_Click(object sender, EventArgs e)
+        {
+            WebServer.loadIndexFile();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) //设置开机自启动  
+            {
+                MessageBox.Show("设置开机自启动，需要修改注册表", "提示");
+                string path = Application.ExecutablePath;
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                rk2.SetValue("MjpegServer", path);
+                rk2.Close();
+                rk.Close();
+            }
+            else //取消开机自启动  
+            {
+                MessageBox.Show("取消开机自启动，需要修改注册表", "提示");
+                string path = Application.ExecutablePath;
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                rk2.DeleteValue("MjpegServer", false);
+                rk2.Close();
+                rk.Close();
+            }
+        }
+    }
+}
